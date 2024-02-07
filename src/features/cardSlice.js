4e59f0ef055245ae816120b348fb87575462a0e7
Paige <paigehcarroll@gmail.com>
2024-02-07 15:04:37 -0600

    removed redux store, created button that will generate a random card from first 23 tarot cards

--- src/features/cardSlice.js
+++ src/features/cardSlice.js
@@ -1,35 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    loading: false,
-    card: null,
-    error: ""
-};
+// const initialState = {
+//     loading: false,
+//     card: null,
+//     error: ""
+// };
 
-export const fetchCard = () => {
-    Math.floor(Math.random() * 22)
-};
+// export const fetchCard = () => {
+//     Math.floor(Math.random() * 22)
+// };
 
 
-const cardSlice = createSlice({
-    name: "card",
-    initialState,
-    reducers: {
-        drawCard: (state) => {
-            state.loading = true;
-        },
-        drawCardSuccess: (state, action) => {
-            state.loading = false;
-            state.card = action.payload;
-            state.error = '';
-        },
-        drawCardFailure: (state, action) => {
-            state.loading = false;
-            state.card = null;
-            state.error = action.payload;
-        }
-    }
-})
+// const cardSlice = createSlice({
+//     name: "card",
+//     initialState,
+//     reducers: {
+//         drawCard: (state) => {
+//             state.loading = true;
+//         },
+//         drawCardSuccess: (state, action) => {
+//             state.loading = false;
+//             state.card = action.payload;
+//             state.error = '';
+//         },
+//         drawCardFailure: (state, action) => {
+//             state.loading = false;
+//             state.card = null;
+//             state.error = action.payload;
+//         }
+//     }
+// })
 
-export const { drawCard, drawCardSuccess, drawCardFailure } = cardSlice.actions;
-export default cardSlice.reducer;
+// export const { drawCard, drawCardSuccess, drawCardFailure } = cardSlice.actions;
+// export default cardSlice.reducer;
